531ba9f9b7c0977419dc73aec2de522b35631476
emiranivala <taitufam@gmail.com>
2025-01-21 18:09:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "-1002262642477")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002466180050"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002437305944"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000000"))
 
